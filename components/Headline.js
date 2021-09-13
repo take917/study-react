056877abf33c501467a99cdbf22143a0aecf592b
growver-code <1tktk2tk3@gmail.com>
2021-09-13 15:11:34 +0900

    ♻️ ヘッドラインをコンポーネント化

---
+++ components/Headline.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Headline(props) {
+  return (
+      <div>
+        <h1 className={styles.title}>
+          {props.page} Page</h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/{props.page}.js</code>
+        </p></div>
+  )
+}
