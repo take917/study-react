25053c488c75f72ae456131412e08f403ff27884
growver-code <1tktk2tk3@gmail.com>
2021-09-29 08:17:13 +0900

    ✨ counterの部分を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,29 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [foo, setFoo] = useState(1);
+
+  const handleClick = (e) => {
+    setFoo((foo) => foo + 1);
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <button>ボタン</button>
+      <h1>{foo}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
